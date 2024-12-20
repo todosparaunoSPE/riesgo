3facafd0b13818a1068c59a972e5d9e1011b584b
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-12-20 17:41:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,22 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score, confusion_matrix, classification_report
 import plotly.express as px
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 st.title("Predicción de Riesgo de Clientes")
 
 # Carga automática del archivo datos_clientes.csv
